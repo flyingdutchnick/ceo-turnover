ab8c9814751eed534159f1d55b16d8be0b48587a
OzziAlk <alkhayat@wharton.upenn.edu>
2020-03-19 03:05:03 -0400

    dev.py now imports IntegrateData classes from package files and uses them to construct master dataset in one file

--- data/dev/dev.py
+++ data/dev/dev.py
@@ -1,6 +1,18 @@
-import numpy as np
-import pandas as pd
+from sp500_data import IntegrateSPData
+from volatility_data import IntegrateVolatilityData
+from sp500_data_merge import MergeSPWithMaster
 
-df = pd.read_csv("csv_files/master_data2000.csv")
 
-print(df.columns)
+initial_master = "csv_files/master_data_monthly.csv"
+intermediate_master = "csv_files/master_data_monthly_temp.csv"
+final_master = "csv_files/master_data_monthly_final.csv"
+sp500_raw = "csv_files/SP500-Monthly-2000"
+sp500_processed = "csv_files/SP500-Returns-Volatility.csv"
+
+volatility_data = IntegrateVolatilityData(initial_master, intermediate_master)
+sp_data = IntegrateSPData(sp500_raw, sp500_processed)
+master_data = MergeSPWithMaster(intermediate_master, sp500_processed, final_master)
+
+volatility_data.process()
+sp_data.process()
+master_data.process()
