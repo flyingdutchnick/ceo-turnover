0c8f34e720205ae3574dc451c17620aff5b32077
OzziAlk <alkhayat@wharton.upenn.edu>
2020-03-19 12:47:13 -0400

    Added functionality to change monthly dataset -> yearly dataset

---
+++ data/dev/make_data_yearly.py
@@ -0,0 +1,27 @@
+from dev_lib import IntegrateData
+
+
+class MakeYearly(IntegrateData):
+
+    def __init__(self, in_file, out_file, date_col='date', date_form='YYYYMMDD'):
+        self.date_column = date_col
+        self.date_format = date_form
+        super().__init__(in_file, out_file)
+
+    def integrate_data(self):
+        if self.date_form == 'YYYYMMDD':
+            self.data.drop(self.data[(self.data['date'] // 100) % 100 != 1].index, inplace=True)
+        else:
+            print("Unrecognized date format")
+            return
+
+        super().integrate_data()
+
+
+def main():
+    in_path = "csv_files/master_data_monthly_v2.csv"
+    out_path = "csv_files/master_data_yearly_v0.csv"
+
+    data = MakeYearly(in_path, out_path)
+    data.process()
+
