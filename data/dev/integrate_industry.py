2c3f4d6de68e863b0d4609855768d49dab4844d2
OzziAlk <alkhayat@wharton.upenn.edu>
2020-03-25 02:39:41 -0400

    Added integrate_industry.py

---
+++ data/dev/integrate_industry.py
@@ -0,0 +1,33 @@
+from dev_lib import IntegrateData
+from pandas import read_csv, merge
+
+
+class IntegrateIndustry(IntegrateData):
+
+    def __init__(self, in_master, out_master, industry_map, id_col='gvkey'):
+        self.industry_path = industry_map
+        self.industry_data = None
+        self.id = id_col
+        super().__init__(in_master, out_master)
+
+    def integrate_data(self):
+        self.industry_data.drop_duplicates(subset=self.id, inplace=True)
+        self.data = merge(self.data, self.industry_data, how='inner', left_on=self.id, right_on=self.id)
+        super().integrate_data()
+
+    def read_data(self):
+        self.industry_data = read_csv(self.industry_path)
+        super().read_data()
+
+
+def main():
+
+    in_path = "csv_files/master_data_yearly_v0.csv"
+    out_path = "csv_files/master_data_yearly_v1.csv"
+    industry_map = "csv_files/CompustatIDMappings.csv"
+
+    data = IntegrateIndustry(in_path, out_path, industry_map)
+    data.process()
+
+
+main()
