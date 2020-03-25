9209338e1f3486ff385fb5cba47769052a263937
OzziAlk <alkhayat@wharton.upenn.edu>
2020-03-25 00:31:00 -0400

    Added FactoryIntegrateData to make_data_yearly file

--- data/dev/make_data_yearly.py
+++ data/dev/make_data_yearly.py
@@ -1,6 +1,7 @@
 from dev_lib import IntegrateData, FactoryIntegrateData
 
 
+# Define an IntegrateData subclass to transform monthly data into yearly data
 class MakeYearly(IntegrateData):
 
     def __init__(self, in_file, out_file, date_col='date', date_form='YYYYMMDD'):
@@ -20,15 +21,24 @@ class MakeYearly(IntegrateData):
         super().integrate_data()
 
 
-def monthly_to_yearly(data):
-    return data.drop(data[(data['date'] // 100) % 100 != 1].index)
+# Example of the FactoryIntegrateData class in action, this is functionally equivalent to main()
+def factory_integrate_example():
+    in_path = "csv_files/master_data_monthly_v2.csv"
+    out_path = "csv_files/master_data_yearly_v0.csv"
+
+    def monthly_to_yearly(df):
+        return df.drop(df[(df['date'] // 100) % 100 != 1].index)
+
+    data = FactoryIntegrateData(in_path, out_path, monthly_to_yearly)
+    data.process()
 
 
+# Execute the data transform from monthly to yearly
 def main():
     in_path = "csv_files/master_data_monthly_v2.csv"
     out_path = "csv_files/master_data_yearly_v0.csv"
 
-    data = FactoryIntegrateData(in_path, out_path, monthly_to_yearly)
+    data = MakeYearly(in_path, out_path)
     data.process()
 
 
