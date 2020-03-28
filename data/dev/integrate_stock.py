da30022caf2478ad70b82863bfd952bf61b8d870
OzziAlk <alkhayat@wharton.upenn.edu>
2020-03-28 18:37:28 -0400

    updated date handling

--- data/dev/integrate_stock.py
+++ data/dev/integrate_stock.py
@@ -25,7 +25,7 @@ class IntegrateStockData(IntegrateData):
 
         def make_data_sortable(date):
             days_input = date[0:2]
-            days = int(days_input.replace('\\', ''))
+            days = int(days_input.replace('/', ''))
             month = month_str_to_num[date[2:5]]
             year = int(date[5:9])
 
