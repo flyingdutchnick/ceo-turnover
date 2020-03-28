aa78bd6dbb31f7c47900b813f544e11614fa66ff
OzziAlk <alkhayat@wharton.upenn.edu>
2020-03-28 19:32:30 -0400

    addressed date format issue for integrate_stock

--- data/dev/integrate_stock.py
+++ data/dev/integrate_stock.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from dev_lib import IntegrateData, pct_chg
 
 
@@ -20,29 +21,14 @@ class IntegrateStockData(IntegrateData):
         super().integrate_data()
 
     def construct_date_from_datadate(self):
-        month_str_to_num = {"JAN": 1, "FEB": 2, "MAR": 3, "APR": 4, "MAY": 5, "JUN": 6, "JUL": 7,
-                            "AUG": 8, "SEP": 9, "OCT": 10, "NOV": 11, "DEC": 12}
-
-        def make_data_sortable(date):
-            days_input = date[0:2]
-            print(days_input)
-            days = int(days_input.replace('/', ''))
-            month = month_str_to_num[date[2:5]]
-            year = int(date[5:9])
-
-            return days + month * 100 + year * 10000
-
-        self.data['date'] = self.data['datadate'].apply(lambda x: make_data_sortable(x))
+        self.data['date'] = pd.to_datetime(self.data['datadate'], format='%m/%d/%Y')
 
         self.data.sort_values(by=['sic', 'gvkey', 'date'], inplace=True)
         self.data.reset_index(drop=True, inplace=True)
 
     def trailing_n_months_return(self, row, n=12):
         if row < n:
-            return 0
-
-        trailing_date = self.data.loc[row - n, 'date']
-        current_date = self.data.loc[row, 'date']
+            return "NaN"
 
         trailing_price = self.data.loc[row - n, 'prccm']
         current_price = self.data.loc[row, 'prccm']
@@ -50,26 +36,15 @@ class IntegrateStockData(IntegrateData):
         trailing_id = self.data.loc[row - n, 'gvkey']
         current_id = self.data.loc[row, 'gvkey']
 
-        if IntegrateStockData.month_difference(current_date, trailing_date) == n and trailing_id == current_id:
+        if trailing_id == current_id:
             return pct_chg(current_price, trailing_price)
         else:
-            return 0
+            return "NaN"
 
-    @staticmethod
-    def month_difference(date_1, date_2):
-        month = lambda date: (date // 100) % 100
-        year = lambda date: date // 10000
-
-        return ((year(date_1) - year(date_2)) * 12) + (month(date_1) - month(date_2))
-
-
-def main():
-
-    file_in = "csv_files/SharePriceDataHead.csv"
-    file_out = "csv_files/StockReturnsHead.csv"
-
-    data = IntegrateStockData(file_in, file_out)
-    data.process()
-
-    print()
+    # @staticmethod
+    # def month_difference(date_1, date_2):
+    #     month = lambda date: (date // 100) % 100
+    #     year = lambda date: date // 10000
+    #
+    #     return ((year(date_1) - year(date_2)) * 12) + (month(date_1) - month(date_2))
 
